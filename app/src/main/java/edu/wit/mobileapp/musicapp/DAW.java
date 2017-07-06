4f3221f5bc39b4f2ea27c2262934437f57e0c199
CALICAWESOME <greendolphinavenue@gmail.com>
2017-07-06 18:16:28 -0400

    add more DAW stuff, suggestion trie.

--- app/src/main/java/edu/wit/mobileapp/musicapp/DAW.java
+++ app/src/main/java/edu/wit/mobileapp/musicapp/DAW.java
@@ -2,6 +2,7 @@ package edu.wit.mobileapp.musicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,14 +27,34 @@ public class DAW extends AppCompatActivity {
             InputStream is = getAssets().open("prog.txt");
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // do stuff
+            String l;
+            while ((l = reader.readLine()) != null) {
+                String[] ll = l.split(" ");
+                int[] prog = new int[ll.length];
+                for (int i = 0; i < ll.length; i++) {
+                    prog[i] = Integer.parseInt(ll[i]);
+                }
+                insertRecursive(root, prog, 0);
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void insertRecursive(Node curr, int[] line, int i) {
+        // base case, empty string means parent is the last letter in a word
+        if (i == line.length) {
+            curr.end = true;
+            return;
+        }
+        int j = line[i]-1; // j is the index of the interval, not the interval itself.
+        // if next[j] is null, put a node there
+        if (curr.next[j] == null) {
+            curr.next[j] = new Node();
+        }
+        // do the deed
+        insertRecursive(curr.next[j], line, i+1);
     }
 
     @Override
