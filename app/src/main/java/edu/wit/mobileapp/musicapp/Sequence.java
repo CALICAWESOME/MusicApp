ee91b15efdbcc5642c2614cd245e32769475708b
Cannistraro <cannistrarod@wit.edu>
2017-08-02 09:36:26 -0400

    sequence update

--- app/src/main/java/edu/wit/mobileapp/musicapp/Sequence.java
+++ app/src/main/java/edu/wit/mobileapp/musicapp/Sequence.java
@@ -14,6 +14,7 @@ public class Sequence {
     //array of  64 arraylists of mediaplayer sounds to play at the given step
     private ArrayList<MediaPlayer>[] sounds;
     private int bpm;
+    private int numOfMeasures = 4;
     private float tick;
     private float currentTime = 0;
     private boolean isPlaying = false;
@@ -21,31 +22,52 @@ public class Sequence {
     public Sequence(){
         bpm = 120;
         tick = (float) 250.0*60/bpm;
-        sounds = (ArrayList<MediaPlayer>[])new ArrayList[64];
+        sounds = (ArrayList<MediaPlayer>[])new ArrayList[numOfMeasures*16];
     }
 
     public Sequence(int bpm){
         this.setBpm(bpm);
-        sounds = (ArrayList<MediaPlayer>[])new ArrayList[64];
+        sounds = (ArrayList<MediaPlayer>[])new ArrayList[numOfMeasures*16];
+    }
+
+    public Sequence(int measures, int bpm){
+        this.setBpm(bpm);
+        numOfMeasures = measures;
+        sounds = (ArrayList<MediaPlayer>[])new ArrayList[numOfMeasures*16];
     }
 
     public void setBpm(int x){
-        if (isPlaying){
-            this.pause();
-        }
+        this.pause();
+
         bpm = x;
         tick = 250f*60f/bpm;
     }
 
     //adds a mediaplayer sound object that will play at step t (Ex: if t = 4, the sound will play on the 2nd beat)
     public void addSound(MediaPlayer x, int t){
+        this.pause();
         sounds[t].add(x);
     }
 
     //multiple sounds at one given time
     public void addSounds(MediaPlayer[] x, int t){
+        this.pause();
         for (MediaPlayer m : x) {
             this.addSound(m, t);
+        }
+    }
+
+    public void removeSound(MediaPlayer x, int t){
+        this.pause();
+        sounds[t].remove(x);
+    }
+
+    public void clearSequence(){
+        this.pause();
+        for (int i=0;i<sounds.length;i++){
+            if (sounds[i] != null){
+                sounds[i].clear();
+            }
         }
     }
 
@@ -61,7 +83,7 @@ public class Sequence {
         }
     }
 
-    //not actually, actually stop
+    //not actually pause, actually stop
     public void pause(){
         if (!isPlaying){
             return;
@@ -76,5 +98,9 @@ public class Sequence {
             //kill thread
             isPlaying = false;
         }
+    }
+
+    public boolean isPlaying(){
+        return isPlaying;
     }
 }
